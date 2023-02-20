014a595a1cfa631f14ac370576870c1b9d975726
Alhassan Osman <oalhassan847@gmail.com>
2023-02-20 20:11:51 +0000

    Add TodosLogic component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import TodosLogic from "@/components/TodosLogic";
 
 const TodoApp = () => {
     return (
         <>
             <Header />
+            <TodosLogic />
         </>
     );
 };
