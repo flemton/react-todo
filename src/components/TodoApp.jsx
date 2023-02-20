f4af327c904f3db0c9c92b89aa40b6db344fa446
Alhassan Osman <oalhassan847@gmail.com>
2023-02-20 20:09:55 +0000

    Add header component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,10 @@
+import Header from "@/components/Header";
+
 const TodoApp = () => {
     return (
-      <div>
-        <h1>Hello world!</h1>
-        <p>I am in a React Component!</p>
-      </div>
+        <>
+            <Header />
+        </>
     );
-  };
-  export default TodoApp;
-  
+};
+export default TodoApp;
