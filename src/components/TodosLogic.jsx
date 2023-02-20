f3816e7c975dc0a95b094a3afaf2425abb4bb873
Alhassan Osman <oalhassan847@gmail.com>
2023-02-20 20:31:02 +0000

    Add InputTodo component

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,7 +1,28 @@
 const TodosLogic = () => {
+    const todos = [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ];
     return (
-      <div>TodosLogic content</div>
-    )
+        <ul>
+        {todos.map((todo) => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
+    );
   }
   export default TodosLogic;
   
