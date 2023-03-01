7daf15bfb4ebdc0039e4a6d35cc5c2765f405f83
Osman Alhassan <oalhassan847@gmail.com>
2023-03-01 10:13:15 +0000

    Set state to update item completed bool value

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,24 @@
-const TodoItem = ({ itemProp }) => {
+const TodoItem = ({ itemProp, setTodos }) => {
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !itemProp.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
   return <li>
-    <input type="checkbox" />
+    <input
+      type="checkbox"
+      checked={itemProp.completed}
+      onChange={() => handleChange(itemProp.id)}
+    />
     {itemProp.title}
   </li>;
 };
--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,28 +1,29 @@
 import InputTodo from '@/components/InputTodo';
 import TodosList from './TodosList';
+import { useState } from 'react';
 
 const TodosLogic = () => {
-    const todos = [
+    const [todos, setTodos] = useState([
         {
-            id: 1,
-            title: 'Setup development environment',
-            completed: true,
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
         },
         {
-            id: 2,
-            title: 'Develop website and add content',
-            completed: false,
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
         },
         {
-            id: 3,
-            title: 'Deploy to live server',
-            completed: false,
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
         },
-    ];
+      ]);
     return (
         <div>
             <InputTodo />
-            <TodosList todosProps={todos} />
+            <TodosList todosProps={todos} setTodos={setTodos}/>
         </div>
     );
 }
